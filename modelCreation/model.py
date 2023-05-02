24d5c94a6c5d835e63b6633b6d867601ba2121b4
David Fales <davidfalesct@gmail.com>
2023-05-02 16:14:53 -0600

    added frontend and model creation

---
+++ modelCreation/model.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+
+print(pd)
+print(np)
+print(tree)
+
